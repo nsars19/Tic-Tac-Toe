3420b787e9c27776ad63514a5b9d9ccbf2d7bc8f
Nick <nsardonini@gmail.com>
2020-04-01 18:45:13 -0400

    add #over? and #check_results to Board class

--- classes.rb
+++ classes.rb
@@ -4,10 +4,40 @@ module Classes
     
     def initialize
       @grid = [
-                ["", "", ""],
-                ["", "", ""],
-                ["", "", ""]
+                [" ", " ", " "],
+                [" ", " ", " "],
+                [" ", " ", " "]
       ]
+    end
+
+    private
+    def over?
+      
+    end
+
+    def check_results
+      rows = []
+      columns = []
+      diagonals = []
+      # Populates array with row contents as strings
+      3.times do |i|
+        rows << self.grid[i].join
+      end
+      # Populates array with column contents as strings
+      3.times do |i|
+        col_temp = []
+        3.times do |j|
+          col_temp << self.grid[j][i]
+        end
+        columns << col_temp.join
+      end
+      # Populates array with diagonal contents as strings
+      diag_temp = []
+      3.times do |i|
+        diag_temp << self.grid[i][i]
+      end
+      diagonals << diag_temp.join
+      diagonals << "#{self.grid[0][2]}#{self.grid[1][1]}#{self.grid[2][0]}"
     end
   end
 
@@ -34,3 +64,4 @@ module Classes
     end
   end
 end
+
