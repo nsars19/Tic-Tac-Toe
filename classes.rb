495f0c3bda1381043120a774df601552a9f2e27b
Nick <nsardonini@gmail.com>
2020-04-01 16:13:26 -0400

    add classes to module Classes

--- classes.rb
+++ classes.rb
@@ -1,22 +1,24 @@
-class Cell
-  @@chosen_cells = {}
+module Classes
+  class Cell
+    @@chosen_cells = {}
 
-  def initialize number, player
-    @number = number
-    @@chosen_cells[number] = player
+    def initialize number, player
+      @number = number
+      @@chosen_cells[number] = player
+    end
+
+    def self.chosen_cells
+      @@chosen_cells
+    end
   end
 
-  def self.chosen_cells
-    @@chosen_cells
-  end
-end
+  class Player
+    def initialize name
+      @name = name
+    end
 
-class Player
-  def initialize name
-    @name = name
-  end
-
-  def choose_cell num 
-    Cell.new(num, @name)
+    def choose_cell num 
+      Cell.new(num, @name)
+    end
   end
 end
