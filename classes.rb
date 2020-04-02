d4de8922663b6bb92053c00181494d19a2680c4b
Nick <nsardonini@gmail.com>
2020-04-02 16:02:04 -0400

    change #over? logic. create #display_board

--- classes.rb
+++ classes.rb
@@ -13,8 +13,31 @@ module Classes
       ]
     end
 
+    def over?
+      check_if_win
+      3.times do |i|
+        return true if @rows[i]      == "XXX" || @rows[i]      == "OOO"
+        return true if @columns[i]   == "XXX" || @columns[i]   == "OOO"
+        return true if @diagonals[i] == "XXX" || @diagonals[i] == "OOO"
+      end
+      return true if Cell.count == 9
+      false
+    end
+
+    def display_board
+      cell = self.grid
+      puts "#{cell[0][0]} | #{cell[0][1]} | #{cell[0][2]}"
+      puts "----------"
+      puts "#{cell[1][0]} | #{cell[1][1]} | #{cell[1][2]}"
+      puts "----------"
+      puts "#{cell[2][0]} | #{cell[2][1]} | #{cell[2][2]}"
+    end
+
     private
-    def check_results
+    def check_if_win
+      @rows = []
+      @columns = []
+      @diagonals = []
       # Populates array with row contents as strings
       3.times do |i|
         @rows << self.grid[i].join
@@ -35,23 +58,12 @@ module Classes
       @diagonals << diag_temp.join
       @diagonals << "#{self.grid[0][2]}#{self.grid[1][1]}#{self.grid[2][0]}"
     end
-
-    def over?
-      return true if @rows == "XXX" || @rows == "OOO"
-      return true if @columns == "XXX" || @columns == "OOO"
-      return true if @diagonals == "XXX" || @diagonals == "OOO"
-      return true if Cell.count == 9
-      false
-    end
   end
 
   class Cell
-    @@chosen_cells = {}
     @@count = 0
 
-    def initialize number, player
-      @number = number
-      @@chosen_cells[number] = player
+    def initialize
       @@count += 1
     end
 
@@ -65,12 +77,18 @@ module Classes
   end
 
   class Player
+    attr_reader :name
+
     def initialize name
       @name = name
     end
 
     def choose_cell num 
-      Cell.new(num, @name)
+      case num
+      when 1 || 2 || 3
+        Cell.new
+        
+      end
     end
   end
 end
